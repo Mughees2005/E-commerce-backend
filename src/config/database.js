17fe0ea200919a0ae2ff4d9045117d1ee460c1db
Mughees <smughees78@gmail.com>
2026-02-17 12:05:30 +0500

    Revert "add PostgreSQL connection setup"
    This reverts commit 6928916ffbfd9f7bc0f2e4f871eec5a089a863f8.
    
    Revert database config change

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const {sequelize} = require('sequelize')
-
-const sequelize = new Sequelize('ecommerce', 'admin', 'admin123', {
-    host: 'localhost',
-    dialect: 'postgres',
-    port: 5432,
-    logging: false
-});
-module.exports = sequelize;
