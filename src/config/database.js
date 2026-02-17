1b38a68b313ddd4902bcc0a0a74de59e248cc573
Mughees <smughees78@gmail.com>
2026-02-17 12:18:36 +0500

    add Sequelize PostgreSQL connection setup

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const {Sequelize} = require('sequelize')
+
+const sequelize = new Sequelize('ecommerce', 'admin', 'admin123', {
+    host: 'localhost',
+    dialect: 'postgres',
+    port: 5432,
+    logging: false
+});
+module.exports = sequelize;
