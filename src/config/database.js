15e8f2b4072bee9351edb71671f4bf39969f21c6
Mughees <smughees78@gmail.com>
2026-02-28 16:13:36 +0500

    Update database port from 5432 to 5441

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const {Sequelize} = require('sequelize')
 const sequelize = new Sequelize('ecommerce', 'admin', 'admin123', {
     host: 'localhost',
     dialect: 'postgres',
-    port: 5432,
+    port: 5441,
     logging: false
 });
 module.exports = sequelize;
